6044167ef5f3c835c974fd12a8d7b27fcb3f1e59
andrewgerez <andrewgerez18@gmail.com>
2024-10-23 14:38:40 -0300

    example: dynamic function args with generics

---
+++ src/modules/dynamic-args-with-generics.ts
@@ -0,0 +1,27 @@
+/**
+ * Dynamic function args with generics
+ * 
+ * Use this function when you want to pass a generic type as an argument to a function.
+ * It's especially useful when you have a function that accepts multiple types of arguments.
+ * 
+ */
+
+type EventArgs =
+  | {
+    type: 'LOG_IN'
+    payload: {
+      userId: string
+    }
+  }
+  | {
+    type: 'SIGN_OUT'
+  }
+
+function sendEvent<T extends EventArgs['type']>(
+  ...args: Extract<EventArgs, { type: T }> extends { payload: infer TPayload }
+    ? [type: T, payload: TPayload]
+    : [type: T]
+) { }
+
+sendEvent('LOG_IN', { userId: '1' })
+sendEvent('SIGN_OUT')
