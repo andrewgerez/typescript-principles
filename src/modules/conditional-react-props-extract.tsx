e36534eeca834d596b3097dff8a9402b3c597404
andrewgerez <andrewgerez18@gmail.com>
2024-10-27 02:45:17 -0300

    example: conditional react props extract

---
+++ src/modules/conditional-react-props-extract.tsx
@@ -0,0 +1,25 @@
+/**
+ * Conditional React Props Extract
+ * 
+ * A utility type for extracting the props of a React component base on a condition.
+ */
+
+import React from 'react'
+
+function MyComponent(props: { enabled: boolean }) {
+  return null
+}
+
+class MyOtherComponent extends React.Component<{
+  enabled: boolean
+}> { }
+
+type PropsFrom<T> = T extends React.FC<infer Props>
+  ? Props
+  : T extends React.Component<infer Props>
+  ? Props
+  : never
+
+const props: PropsFrom<MyOtherComponent> = {
+  enabled: true
+}
